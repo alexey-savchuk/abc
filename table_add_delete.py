16952d27d6f38ce1b977ad2398ba8f2cfa6aa8b6
yarovoy <vadim.d.yarovoy@yandex.ru>
2022-10-20 22:04:49 +0300

    default values

--- table_add_delete.py
+++ table_add_delete.py
@@ -4,31 +4,35 @@ dpg.create_context()
 dpg.set_global_font_scale(1.4)
 dpg.create_viewport(title='Coolest table', width=1280, height=720)
 
-
-def add_row():
+def add_row(sender="", app_data="", user_data=""):
     global rows
     with dpg.table_row(parent="Suffer"):
         for i in range(0, columns):
-            dpg.add_text(f"row{rows} column{i}", tag=f"cell_{rows}{i}")
+            dpg.add_text(f"{user_data[i]}", tag=f"cell_{rows}{i}")
             dpg.bind_item_handler_registry(f"cell_{rows}{i}", f"cell_handler_{rows}{i}")
     rows += 1
 
 
-def create_table(colcount=3, rowcount=0):
+def create_table(colcount=3, rowcount=20):
     global columns
     global rows
+
     columns = colcount
     rows = 0
     with dpg.table(parent="main", header_row=True, policy=dpg.mvTable_SizingStretchProp,
                    borders_outerH=True, borders_innerV=True, borders_innerH=True, borders_outerV=True,
-                   tag="Suffer", row_background=True, scrollY=True):
+                   tag="Suffer", row_background=True, scrollY=True, no_host_extendX=True):
         for i in range(0, colcount):
             dpg.add_table_column(tag=f"col_{i}", label=f"column {i}")
+
+        for i in range(rowcount):
+            add_row(user_data=["----------" for i in range(3)])
+
 
 with dpg.window(tag="main"):
     dpg.add_text("Step by step mode")
     dpg.add_spacer()
-    dpg.add_button(tag="add_row_button", label="add row", callback=add_row)
+    dpg.add_button(tag="add_row_button", label="add row", callback=add_row, user_data=["one", "two", "three"])
 
     create_table()
 
